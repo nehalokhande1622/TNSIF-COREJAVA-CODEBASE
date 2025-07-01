cb69d6eeeea641c0222d0cad0603182a14368b88
LENOVO <LENOVO@LAPTOP-36GV900V>
2025-07-01 19:25:50 +0530

    Day2

---
+++ Day 2/src/tnsif/day2/IfElseIf.java
@@ -0,0 +1,21 @@
+package tnsif.day2;
+
+public class IfElseIf {
+public static void main(String[] args) {
+		
+		int age =55;
+		
+		if(age >= 18)
+		{ 
+			System.out.println("you are eligible for voting ");
+		}
+		else
+		{
+			System.out.println("Sorry ! You need to wait more ");
+		}
+
+	}
+	
+	
+
+}
