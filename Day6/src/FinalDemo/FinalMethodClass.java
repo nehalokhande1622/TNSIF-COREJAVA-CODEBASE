6fb338d8edaf3b91ef0bb6d1e6deb113a0c0b8cb
LENOVO <LENOVO@LAPTOP-36GV900V>
2025-07-05 18:13:15 +0530

    Day6

---
+++ Day6/src/FinalDemo/FinalMethodClass.java
@@ -0,0 +1,18 @@
+package FinalDemo;
+
+public class FinalMethodClass {
+	
+	public FinalMethodClass() {
+		System.out.println("this is a default constructor ");
+	}
+	
+	final int a=50;
+	 // final method 
+	final void show()
+	{
+		System.out.println("value of a "+ a);
+	}
+	
+	
+
+}
