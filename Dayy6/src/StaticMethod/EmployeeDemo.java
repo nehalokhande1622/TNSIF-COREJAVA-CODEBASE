80189edb4dda0383c2293e18506ef94f3148ce82
LENOVO <LENOVO@LAPTOP-36GV900V>
2025-07-06 11:21:26 +0530

    Day6

---
+++ Dayy6/src/StaticMethod/EmployeeDemo.java
@@ -0,0 +1,12 @@
+package StaticMethod;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		
+		Employee e=new Employee("Shubham", 1001);
+		System.out.println(e);
+
+	}
+
+}
