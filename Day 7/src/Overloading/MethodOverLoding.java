504e9f89324822279a10c8adefe28e5ac1ab50c0
LENOVO <LENOVO@LAPTOP-36GV900V>
2025-07-07 17:47:05 +0530

    Day7

---
+++ Day 7/src/Overloading/MethodOverLoding.java
@@ -0,0 +1,30 @@
+package Overloading;
+
+public class MethodOverLoding {
+	
+	public static int addition(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public static int addition(int a, int b, int c)
+	{
+		return a+b+c;
+	}
+	public static float addition(float a, float b)
+	{
+		return a+b;
+	}
+	public static float addition(float a, int b)
+	{
+		return a+b;
+	}
+	
+	public static float addition(int a, float b)
+	{
+		return a+b;
+	}
+	
+	
+
+}
---
+++ Day7/snippet/Snippet.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		
+			Student shreya=new Student();
+			shreya.isRegistered();
+	
+		}
+	
+	}
+}
+
